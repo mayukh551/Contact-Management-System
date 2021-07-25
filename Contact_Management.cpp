cbeb6493781c8900e4fbe325b067ebf4fb9f88d9
Abesh1903 <86250766+Abesh1903@users.noreply.github.com>
2021-07-25 22:07:15 +0530

    Update Contact_Management.cpp

--- Contact_Management.cpp
+++ Contact_Management.cpp
@@ -289,7 +289,7 @@ struct contact * upperBoundary (struct contact* lb, struct contact* ub, char str
 		return 0;
 	}
 	
-	else  //From here...
+	else  //FROM HERE...
 	{
 		while (ptr != ub->next)
 		{
@@ -312,7 +312,7 @@ struct contact * upperBoundary (struct contact* lb, struct contact* ub, char str
     		p = p->next;
 		}
 		return 0;
-	} //...Till
+	} //...TILL
 	
 	
 	
